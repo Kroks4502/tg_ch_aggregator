e12f536f4e5db5f6ac6a5fad672ca20a49c30a27
Kroks <kroks4502@gmail.com>
2022-09-17 11:11:19 +0300

    Рефакторинг методов моделей

--- models.py
+++ models.py
@@ -20,60 +20,62 @@ class Source(BaseModel):
     tg_id = IntegerField(unique=True)
     title = CharField()
     category = ForeignKeyField(
-        Category, backref='channels', on_delete='CASCADE')
+        Category, backref='sources', on_delete='CASCADE')
 
     def __str__(self):
         return self.title
 
-    def get_blacklist_hashtag(self) -> list:
-        query = Filter.filter(content_type='hashtag', source=self)
-        return [blacklist.pattern for blacklist in query]
+    def get_filter_patterns(self, content_type):
+        query = Filter.filter(content_type=content_type, source=self)
+        return [f.pattern for f in query]
 
-    def get_blacklist_part_of_url(self) -> list:
-        query = Filter.filter(content_type='part_of_url', source=self)
-        return [blacklist.pattern for blacklist in query]
+    def get_filter_hashtag(self) -> list:
+        return self.get_filter_patterns('hashtag')
 
-    def get_blacklist_part_of_text(self) -> list:
-        query = Filter.filter(content_type='part_of_text', source=self)
-        return [blacklist.pattern for blacklist in query]
+    def get_filter_part_of_url(self) -> list:
+        return self.get_filter_patterns('part_of_url')
 
-    def get_blacklist_reply_markup(self) -> list:
-        query = Filter.filter(content_type='reply_markup', source=self)
-        return [blacklist.pattern for blacklist in query]
+    def get_filter_part_of_text(self) -> list:
+        return self.get_filter_patterns('part_of_text')
+
+    def get_filter_reply_markup(self) -> list:
+        return self.get_filter_patterns('reply_markup')
 
     @staticmethod
     def get_all_ids():
-        return [ch.tg_id for ch in Source.select(Source.tg_id)]
+        return [s.tg_id for s in Source.select(Source.tg_id)]
 
 
-FILTER_CONTENT_TYPES = ('hashtag', 'part_of_url', 'part_of_text')
+FILTER_CONTENT_TYPES = ('hashtag', 'part_of_url', 'part_of_text',
+                        'reply_markup')
 
 
 class Filter(BaseModel):
     pattern = CharField()
     content_type = CharField()
     source = ForeignKeyField(
-        Source, null=True, backref='blacklists', on_delete='CASCADE')
+        Source, null=True, backref='filters', on_delete='CASCADE')
 
     def __str__(self):
         return self.pattern
 
-    @staticmethod
-    def global_hashtag() -> list:
-        query = Filter.filter(content_type='hashtag', source=None)
-        return [blacklist.pattern for blacklist in query]
+    @classmethod
+    def get_global_patterns(cls, content_type) -> list:
+        query = cls.filter(content_type=content_type, source=None)
+        return [f.pattern for f in query]
 
-    @staticmethod
-    def global_part_of_url() -> list:
-        query = Filter.filter(content_type='part_of_url', source=None)
-        return [blacklist.pattern for blacklist in query]
+    @classmethod
+    def global_hashtag_patterns(cls) -> list:
+        return cls.get_global_patterns('hashtag')
 
-    @staticmethod
-    def global_part_of_text() -> list:
-        query = Filter.filter(content_type='part_of_text', source=None)
-        return [blacklist.pattern for blacklist in query]
+    @classmethod
+    def global_part_of_url_patterns(cls) -> list:
+        return cls.get_global_patterns('part_of_url')
 
-    @staticmethod
-    def global_reply_markup() -> list:
-        query = Filter.filter(content_type='reply_markup', source=None)
-        return [blacklist.pattern for blacklist in query]
+    @classmethod
+    def global_part_of_text_patterns(cls) -> list:
+        return cls.get_global_patterns('part_of_text')
+
+    @classmethod
+    def global_reply_markup_patterns(cls) -> list:
+        return cls.get_global_patterns('reply_markup')
--- plugins/user/checks.py
+++ plugins/user/checks.py
@@ -10,29 +10,29 @@ from models import Source, Filter
 async def is_passed_filter(message: Message):
     text = message.text or message.caption
     entities = message.entities or message.caption_entities
-    channel = Source.get(tg_id=message.chat.id)
+    source = Source.get(tg_id=message.chat.id)
     if entities:
         for entity in entities:
             if entity.type == MessageEntityType.HASHTAG:
                 entity_text = text[entity.offset:entity.offset + entity.length]
-                for hashtag in (Filter.global_hashtag()
-                                + channel.get_blacklist_hashtag()):
+                for hashtag in (Filter.global_hashtag_patterns()
+                                + source.get_filter_hashtag()):
                     if re.search(hashtag, entity_text, flags=re.IGNORECASE):
                         comment = f'hashtag: `{hashtag}`'
                         logger.debug(comment)
                         return False, comment
 
             elif entity.type == MessageEntityType.TEXT_LINK:
-                for part_url in (Filter.global_part_of_url()
-                                 + channel.get_blacklist_part_of_url()):
+                for part_url in (Filter.global_part_of_url_patterns()
+                                 + source.get_filter_part_of_url()):
                     if re.search(part_url, entity.url, flags=re.IGNORECASE):
                         comment = f'part_of_url: `{part_url}`'
                         logger.debug(comment)
                         return False, comment
 
             elif entity.type == MessageEntityType.URL:
-                for part_url in (Filter.global_part_of_url()
-                                 + channel.get_blacklist_part_of_url()):
+                for part_url in (Filter.global_part_of_url_patterns()
+                                 + source.get_filter_part_of_url()):
                     if re.search(
                             part_url,
                             text[entity.offset:entity.offset+entity.length],
@@ -50,14 +50,14 @@ async def is_passed_filter(message: Message):
                 return False, comment
 
     if text:
-        for part_text in (Filter.global_part_of_text()
-                          + channel.get_blacklist_part_of_text()):
+        for part_text in (Filter.global_part_of_text_patterns()
+                          + source.get_filter_part_of_text()):
             if re.search(part_text, text, flags=re.IGNORECASE):
                 comment = f'part_of_text: `{part_text}`'
                 logger.debug(comment)
                 return False, comment
 
-    if Filter.global_reply_markup() + channel.get_blacklist_reply_markup():
+    if Filter.global_reply_markup_patterns() + source.get_filter_reply_markup():
         if message.reply_markup:
             comment = f'reply_markup'
             logger.debug(comment)
