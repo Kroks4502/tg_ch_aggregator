61c1476b002afa0b1a7ed3f99818f85c5573f592
Kroks <kroks4502@gmail.com>
2022-10-07 19:24:26 +0300

    рефакторинг

--- plugins/bot/menu/helpers/senders.py
+++ plugins/bot/menu/helpers/senders.py
@@ -8,14 +8,13 @@ from models import Admin
 async def send_message_to_admins(
         client: Client, callback_query: CallbackQuery, text):
     f_user = callback_query.from_user
-    for admin in Admin.select():
-        if f_user.id != admin.tg_id:
-            if f_user.username:
-                b_text = f'@{f_user.username}'
-            else:
-                full_name = (
-                    f'{f_user.first_name + " " if f_user.first_name else ""}'
-                    f'{f_user.last_name if f_user.last_name else ""}')
-                b_text = f'{full_name} ({f_user.id})' if full_name else f'{f_user.id}'
-            await client.send_message(
-                admin.tg_id, f'{b_text}\n{text}', disable_web_page_preview=True)
+    for admin in Admin.select().where(Admin.tg_id != f_user.id):
+        if f_user.username:
+            b_text = f'@{f_user.username}'
+        else:
+            full_name = (
+                f'{f_user.first_name + " " if f_user.first_name else ""}'
+                f'{f_user.last_name if f_user.last_name else ""}')
+            b_text = f'{full_name} ({f_user.id})' if full_name else f'{f_user.id}'
+        await client.send_message(
+            admin.tg_id, f'{b_text}\n{text}', disable_web_page_preview=True)
