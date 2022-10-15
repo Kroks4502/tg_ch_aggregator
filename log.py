03cd04ea4c6960c05670ca227b62b4a0da804fe7
Kroks <kroks4502@gmail.com>
2022-10-15 18:09:07 +0300

    разработка

--- log.py
+++ log.py
@@ -8,7 +8,7 @@ os.makedirs(LOGS_DIR, exist_ok=True)
 logging.basicConfig(level=logging.WARNING, format=LOG_FORMAT, encoding='utf-8')
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.INFO)
 handler = RotatingFileHandler(os.path.join(LOGS_DIR, 'app.log'),
                               maxBytes=50000000, backupCount=5, encoding='utf-8')
 handler.setFormatter(logging.Formatter(LOG_FORMAT))
@@ -22,7 +22,7 @@ peewee_handler.setFormatter(logging.Formatter(LOG_FORMAT))
 peewee_logger.addHandler(peewee_handler)
 
 pyrogram_logger = logging.getLogger('pyrogram')
-pyrogram_logger.setLevel(logging.WARNING)
+pyrogram_logger.setLevel(logging.INFO)
 pyrogram_handler = RotatingFileHandler(os.path.join(LOGS_DIR, 'pyrogram.log'),
                                        maxBytes=50000000, backupCount=5, encoding='utf-8')
 pyrogram_handler.setFormatter(logging.Formatter(LOG_FORMAT))
