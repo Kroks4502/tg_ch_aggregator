3d8e9ce2cbb1a9e68307973d603106e34c1dc066
Kroks <kroks4502@gmail.com>
2023-06-03 21:52:45 +0300

    feat: optimize check is_new_post; rename get_category_message_history to is_new_post

--- src/models.py
+++ src/models.py
@@ -129,6 +129,7 @@ class Admin(BaseModel):
 class MessageHistoryModel(BaseModel):
     date = DateTimeField(default=datetime.now)
     source = ForeignKeyField(Source, on_delete='CASCADE')
+    source_chat_id = BigIntegerField()
     source_message_id = BigIntegerField()
     source_message_edited = BooleanField(default=False)
     source_message_deleted = BooleanField(default=False)
--- src/plugins/user/utils/inspector.py
+++ src/plugins/user/utils/inspector.py
@@ -6,7 +6,7 @@ from typing import Iterable
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, MessageEntity
 
-from common import get_message_link, get_shortened_text
+from common import get_shortened_text
 from filter_types import FilterMessageType, FilterType
 from models import CategoryMessageHistory, Filter, Source
 from plugins.user.utils.history import add_to_filter_history
@@ -14,14 +14,14 @@ from plugins.user.utils.history import add_to_filter_history
 
 def is_new_and_valid_post(message: Message, source: Source) -> bool:
     """Сообщения ещё нет в истории и оно проходит фильтр."""
-    if h_obj := get_category_message_history(message, source):
+    if not is_new_post(message, source.category_id):
         logging.info(
             f'Сообщение {message.id} из источника '
             f'{get_shortened_text(message.chat.title, 20)} {message.chat.id} '
             f'{"в составе медиагруппы " + str(message.media_group_id) + " " if message.media_group_id else ""}'
             'уже есть в канале категории'
             f' {get_shortened_text(source.category.title, 20)} {source.category.tg_id}'
-            f'{get_message_link(h_obj.category.tg_id, h_obj.message_id)}'
+            f'ИЛИ было отфильтровано.'
         )
         return False
 
@@ -38,54 +38,56 @@ def is_new_and_valid_post(message: Message, source: Source) -> bool:
     return True
 
 
-def get_category_message_history(
-    message: Message,
-    source: Source,
-) -> CategoryMessageHistory | None:
-    """Получить историю сообщения, если она есть."""
+def is_new_post(message: Message, category_id: int) -> bool:
+    """Проверка уникальности сообщения."""
     if message.forward_from_chat:
         # Сообщение переслано в источник из другого чата
-        forward_source = Source.get_or_none(tg_id=message.forward_from_chat.id)
-        # Проверяем есть ли тот чат в наших источниках
-        if forward_source:
-            # Если есть, то значит сообщение уже может быть в истории по этому чату
-            if h_obj := CategoryMessageHistory.get_or_none(
-                category_id=source.category_id,  # Все проверки выполняем в рамках одной категории
-                source_id=forward_source.id,
-                source_message_id=message.forward_from_message_id,
-                deleted=False,
-            ):
-                return h_obj
+
+        # Сообщение уже может быть в истории по этому чату, если он является источником
+        source_chat_id = message.forward_from_chat.id
+        source_message_id = message.forward_from_message_id
 
         # Проверяем не пересылалось ли уже в других источниках это сообщение
-        if h_obj := CategoryMessageHistory.get_or_none(
-            category_id=source.category_id,
-            forward_from_chat_id=message.forward_from_chat.id,
-            forward_from_message_id=message.forward_from_message_id,
-            deleted=False,
-        ):
-            return h_obj
-
+        forward_from_chat_id = message.forward_from_chat.id
+        forward_from_message_id = message.forward_from_message_id
     else:  # Сообщение не является пересланным
         # Проверяем наличие этого сообщения в истории
-        if h_obj := CategoryMessageHistory.get_or_none(
-            category_id=source.category_id,
-            source_id=source.id,
-            source_message_id=message.id,
-            deleted=False,
-        ):
-            return h_obj
+        source_chat_id = message.chat.id
+        source_message_id = message.id
 
         # Проверяем не получили ли мы это сообщение ранее как пересланное из другого источника
-        if h_obj := CategoryMessageHistory.get_or_none(
-            category_id=source.category_id,
-            forward_from_chat_id=message.chat.id,
-            forward_from_message_id=message.id,
-            deleted=False,
-        ):
-            return h_obj
+        forward_from_chat_id = message.chat.id
+        forward_from_message_id = message.id
 
-    return
+    if not (forward_from_chat_id and forward_from_message_id):
+        # todo: Временная проверка, что forward_from_chat_id и forward_from_message_id всегда есть
+        #   (SQL('TRUE') if forward_from_chat_id else SQL('FALSE'))
+        logging.error(
+            'forward_from_chat_id и forward_from_message_id должны быть всегда для выполнения запроса по индексам!'
+        )
+
+    h_a = CategoryMessageHistory.alias()
+    h_obj = h_a.select().where(
+        (
+            h_a.category_id == category_id
+        )  # Все проверки выполняем в рамках одной категории
+        & (
+            (
+                (h_a.source_chat_id == source_chat_id)
+                & (h_a.source_message_id == source_message_id)
+            )
+            | (
+                (h_a.forward_from_chat_id == forward_from_chat_id)
+                & (h_a.forward_from_message_id == forward_from_message_id)
+            )
+        )
+        & ~h_a.deleted  # Удаленные сообщения не учитываем
+    )  # Работает по индексам
+
+    if h_obj.exists():
+        return False
+
+    return True
 
 
 def check_message_filter(message: Message, source: Source) -> dict | None:
