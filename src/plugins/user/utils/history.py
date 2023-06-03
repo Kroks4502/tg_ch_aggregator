39d10c9c36f967d95ad37fae0531b976bfc12cc3
Kroks <kroks4502@gmail.com>
2023-06-03 12:28:14 +0300

    chore: change attr names; optimize query

--- src/plugins/user/utils/history.py
+++ src/plugins/user/utils/history.py
@@ -4,44 +4,42 @@ from models import CategoryMessageHistory, Filter, FilterMessageHistory, Source
 
 
 def add_to_category_history(
-    original_message: Message,
+    source_message: Message,
     category_message: Message,
     source: Source = None,
     rewritten: bool = False,
 ):
     if not source:
-        source = Source.get(tg_id=original_message.chat.id)
+        source = Source.get(tg_id=source_message.chat.id)
     CategoryMessageHistory.create(
-        source=source,
-        source_message_id=original_message.id,
+        source_id=source.id,
+        source_message_id=source_message.id,
         media_group=(
-            original_message.media_group_id if original_message.media_group_id else ''
+            source_message.media_group_id if source_message.media_group_id else ''
         ),
         forward_from_chat_id=(
-            original_message.forward_from_chat.id
-            if original_message.forward_from_chat
+            source_message.forward_from_chat.id
+            if source_message.forward_from_chat
             else None
         ),
-        forward_from_message_id=original_message.forward_from_message_id,
-        category=source.category,
+        forward_from_message_id=source_message.forward_from_message_id,
+        category_id=source.category_id,
         message_id=category_message.id,
         rewritten=rewritten,
     )
 
 
 def add_to_filter_history(
-    original_message: Message,
+    message: Message,
     filter_obj: Filter | int,
     source: Source = None,
 ):
     if not source:
-        source = Source.get(tg_id=original_message.chat.id)
+        source = Source.get(tg_id=message.chat.id)
 
     FilterMessageHistory.create(
-        source=source,
-        source_message_id=original_message.id,
-        media_group=(
-            original_message.media_group_id if original_message.media_group_id else ''
-        ),
+        source_id=source.id,
+        source_message_id=message.id,
+        media_group=(message.media_group_id if message.media_group_id else ''),
         filter=filter_obj,
     )
