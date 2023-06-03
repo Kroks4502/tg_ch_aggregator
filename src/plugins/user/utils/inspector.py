d00b03541ec8c8f84da645300932ea7b48299059
Kroks <kroks4502@gmail.com>
2023-06-03 12:22:15 +0300

    chore: comment code; fix return types

--- src/plugins/user/utils/inspector.py
+++ src/plugins/user/utils/inspector.py
@@ -1,6 +1,7 @@
 import itertools
 import logging
 import re
+from typing import Iterable
 
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, MessageEntity
@@ -13,7 +14,7 @@ from plugins.user.utils.history import add_to_filter_history
 
 def is_new_and_valid_post(message: Message, source: Source) -> bool:
     """Сообщения ещё нет в истории и оно проходит фильтр."""
-    if h_obj := get_message_history(message, source):
+    if h_obj := get_category_message_history(message, source):
         logging.info(
             f'Сообщение {message.id} из источника '
             f'{get_shortened_text(message.chat.title, 20)} {message.chat.id} '
@@ -24,7 +25,7 @@ def is_new_and_valid_post(message: Message, source: Source) -> bool:
         )
         return False
 
-    if data := get_message_filter(message, source):
+    if data := check_message_filter(message, source):
         add_to_filter_history(message, data['id'], source)
         logging.info(
             f'Сообщение {message.id} из источника '
@@ -37,41 +38,47 @@ def is_new_and_valid_post(message: Message, source: Source) -> bool:
     return True
 
 
-def get_message_history(
+def get_category_message_history(
     message: Message,
     source: Source,
 ) -> CategoryMessageHistory | None:
     """Получить историю сообщения, если она есть."""
     if message.forward_from_chat:
+        # Сообщение переслано в источник из другого чата
         forward_source = Source.get_or_none(tg_id=message.forward_from_chat.id)
+        # Проверяем есть ли тот чат в наших источниках
         if forward_source:
+            # Если есть, то значит сообщение уже может быть в истории по этому чату
             if h_obj := CategoryMessageHistory.get_or_none(
-                category=source.category,
-                source=forward_source,
+                category_id=source.category_id,  # Все проверки выполняем в рамках одной категории
+                source_id=forward_source.id,
                 source_message_id=message.forward_from_message_id,
                 deleted=False,
             ):
                 return h_obj
 
+        # Проверяем не пересылалось ли уже в других источниках это сообщение
         if h_obj := CategoryMessageHistory.get_or_none(
-            category=source.category,
+            category_id=source.category_id,
             forward_from_chat_id=message.forward_from_chat.id,
             forward_from_message_id=message.forward_from_message_id,
             deleted=False,
         ):
             return h_obj
 
-    else:
+    else:  # Сообщение не является пересланным
+        # Проверяем наличие этого сообщения в истории
         if h_obj := CategoryMessageHistory.get_or_none(
-            category=source.category,
-            source=source,
+            category_id=source.category_id,
+            source_id=source.id,
             source_message_id=message.id,
             deleted=False,
         ):
             return h_obj
 
+        # Проверяем не получили ли мы это сообщение ранее как пересланное из другого источника
         if h_obj := CategoryMessageHistory.get_or_none(
-            category=source.category,
+            category_id=source.category_id,
             forward_from_chat_id=message.chat.id,
             forward_from_message_id=message.id,
             deleted=False,
@@ -81,7 +88,7 @@ def get_message_history(
     return
 
 
-def get_message_filter(message: Message, source: Source) -> dict | None:
+def check_message_filter(message: Message, source: Source) -> dict | None:
     """Получить информацию о прохождении фильтра, если сообщение его не проходит."""
 
     inspector = FilterInspector(message, source)
@@ -110,7 +117,7 @@ class FilterInspector:
         self._text = message.text or message.caption
         self._source = source
 
-    def check_message_type(self) -> int | None:
+    def check_message_type(self) -> dict | None:
         for data in self._get_filters(FilterType.MESSAGE_TYPE):
             try:
                 if getattr(
@@ -120,19 +127,19 @@ class FilterInspector:
             except AttributeError as e:
                 logging.error(e, exc_info=True)
 
-    def check_white_text(self) -> int | None:
+    def check_white_text(self) -> dict | None:
         for data in self._get_filters(FilterType.ONLY_WHITE_TEXT):
             if not self._search(data['pattern'], self._text):
                 return data
         return
 
-    def check_text(self) -> int | None:
+    def check_text(self) -> dict | None:
         for data in self._get_filters(FilterType.TEXT):
             if self._search(data['pattern'], self._text):
                 return data
         return
 
-    def check_entities(self, entity: MessageEntity) -> int | None:
+    def check_entities(self, entity: MessageEntity) -> dict | None:
         if result := self._check_entity_type(entity):
             return result
 
@@ -144,15 +151,16 @@ class FilterInspector:
             return self._check_url(entity)
         return
 
-    def _check_entity_type(self, entity: MessageEntity) -> int | None:
+    def _check_entity_type(self, entity: MessageEntity) -> dict | None:
         for data in self._get_filters(FilterType.ENTITY_TYPE):
             try:
                 if entity.type == getattr(MessageEntityType, data['pattern'].upper()):
                     return data
             except AttributeError as e:
                 logging.error(e, exc_info=True)
+        return
 
-    def _check_hashtag(self, entity: MessageEntity) -> int | None:
+    def _check_hashtag(self, entity: MessageEntity) -> dict | None:
         for data in self._get_filters(FilterType.HASHTAG):
             if self._search(
                 data['pattern'],
@@ -161,13 +169,13 @@ class FilterInspector:
                 return data
         return
 
-    def _check_text_link(self, entity: MessageEntity) -> int | None:
+    def _check_text_link(self, entity: MessageEntity) -> dict | None:
         for data in self._get_filters(FilterType.URL):
             if self._search(data['pattern'], entity.url):
                 return data
         return
 
-    def _check_url(self, entity: MessageEntity) -> int | None:
+    def _check_url(self, entity: MessageEntity) -> dict | None:
         for data in self._get_filters(FilterType.URL):
             if self._search(
                 data['pattern'],
@@ -176,7 +184,7 @@ class FilterInspector:
                 return data
         return
 
-    def _get_filters(self, f_type: FilterType):
+    def _get_filters(self, f_type: FilterType) -> Iterable[dict]:
         return itertools.chain(
             Filter.get_cache(source=self._source.id, type=f_type.value),
             Filter.get_cache(source=None, type=f_type.value),
