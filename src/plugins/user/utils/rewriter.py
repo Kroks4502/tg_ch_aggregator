f9d27c271ec8d7e2588cd68e53ac5a55cfd5fd7b
Kroks <kroks4502@gmail.com>
2023-06-11 11:25:47 +0300

    feat: add new func rewrite_message

--- src/plugins/user/utils/rewriter.py
+++ src/plugins/user/utils/rewriter.py
@@ -7,6 +7,76 @@ from pyrogram.types import Message, MessageEntity
 from config import PATTERN_WITHOUT_SMILE
 
 
+def rewrite_message(message: Message) -> None:
+    header, header_entities = get_header(message)
+
+    for entity in message.entities or message.caption_entities or []:
+        # Делаем сдвиг сущностей на размер заголовка
+        entity.offset += len(header)
+
+    if message.media:
+        message.caption = header + (message.caption or '')
+        message.caption_entities = header_entities + (message.caption_entities or [])
+    else:
+        message.text = header + (message.text or '')
+        message.entities = header_entities + (message.entities or [])
+
+
+def get_header(message: Message) -> tuple[str, list[MessageEntity]]:
+    src_text = get_src_text(message)
+    fwd_text = get_fwd_text(message)
+
+    header = src_text + (f'\n{fwd_text}' if fwd_text else '') + '\n\n'
+    header_entities = [
+        MessageEntity(
+            type=MessageEntityType.BOLD,
+            offset=0,
+            length=len(src_text),
+        ),
+        MessageEntity(
+            type=MessageEntityType.TEXT_LINK,
+            offset=0,
+            length=len(src_text),
+            url=message.link,
+        ),
+    ]
+    if fwd_text:
+        header_entities.append(
+            MessageEntity(
+                type=MessageEntityType.TEXT_LINK,
+                offset=len(src_text) + 1,  # len(src_text) + len('\n')
+                length=len(fwd_text),
+                url=(
+                    f'https://t.me/{message.forward_from_chat.username}'
+                    f'/{message.forward_from_message_id}'
+                ),
+            ),
+        )
+    return header, header_entities
+
+
+def get_src_text(message: Message) -> str:
+    title = re.sub(
+        PATTERN_WITHOUT_SMILE,
+        '',
+        message.chat.title,
+    )
+    return (
+        f'Источник: {title if title else message.chat.id}'  # 💬 len(💬)=1, but tg len=2
+    )
+
+
+def get_fwd_text(message: Message) -> str:
+    if message.forward_from_chat:
+        title = re.sub(
+            PATTERN_WITHOUT_SMILE,
+            '',
+            message.forward_from_chat.title,
+        )
+        return f'fwd: {title if title else message.chat.id}'
+    return ''
+
+
 def delete_agent_text_in_message(search_result: Match, message: Message):  # noqa: C901
     separator = '\n\n'
     title = re.sub(
