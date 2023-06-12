5f344ca2cb31c705f3a0cb7e619320512de680d1
Kroks <kroks4502@gmail.com>
2023-06-12 15:17:37 +0300

    chore: remove PATTERN_AGENT

--- src/config.py
+++ src/config.py
@@ -20,14 +20,6 @@ API_HASH = os.getenv('api_hash')
 BOT_TOKEN = os.getenv('bot_token')
 DEVELOP_MODE = os.getenv('develop_mode')
 
-PATTERN_AGENT = re.compile(
-    (
-        r'\s*Д*АН*ОЕ\sСООБЩЕНИЕ[\w\s().,]+ИНОСТРАННОГО\s(АГЕНТА|)*[\s.]*\s*|\s*([\d'
-        r' +]+|)НАСТОЯЩИЙ МАТЕРИАЛ'
-        r'[\w \t\x0B\f\r().,+]+[\r\n]*'
-    ),
-    flags=re.IGNORECASE,
-)
 PATTERN_WITHOUT_SMILE = re.compile(
     r'[^а-яА-ЯЁёa-zA-z0-9 |-]+',
     flags=re.IGNORECASE,
