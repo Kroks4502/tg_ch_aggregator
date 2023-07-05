cb54645cf9f50b3195db8240025673382ee7daf9
Kroks <kroks4502@gmail.com>
2023-07-05 23:10:17 +0300

    feat: created edited_message.py

---
+++ src/plugins/user/sources_monitoring/edited_message.py
@@ -0,0 +1,172 @@
+import json
+import logging
+
+from peewee import DoesNotExist
+from pyrogram import Client
+from pyrogram import errors as pyrogram_errors
+from pyrogram.types import Message
+
+from models import MessageHistory, Source
+from plugins.user.exceptions import (
+    MessageBadRequestError,
+    MessageBaseError,
+    MessageFilteredError,
+    MessageForwardedFromAnySourceError,
+    MessageIdInvalidError,
+    MessageNotFoundOnHistoryError,
+    MessageNotModifiedError,
+    MessageNotOnCategoryError,
+    MessageNotRewrittenError,
+    MessageTooLongError,
+    Operation,
+)
+from plugins.user.sources_monitoring.common import get_filter_id_or_none, set_blocking
+from plugins.user.utils import custom_filters, get_input_media
+from plugins.user.utils.cleanup import cleanup_message
+from plugins.user.utils.rewriter import Header, add_header
+from pyrogram_fork.edit_media_message import EditMessageMedia
+
+EDIT = Operation.EDIT
+
+
+@Client.on_edited_message(
+    custom_filters.monitored_channels,
+)
+async def edit_regular_message(client: Client, message: Message):  # noqa C901
+    logging.debug(
+        'Источник %s изменил сообщение %s',
+        message.chat.id,
+        message.id,
+    )
+
+    source = Source.get(message.chat.id)
+
+    blocked = None
+    history_obj = None
+    exc = None
+    try:
+        blocked = set_blocking(
+            operation=EDIT,
+            message=message,
+            block_value=message.id,
+        )
+
+        history_obj = get_history_obj_or_none(
+            category_id=source.category_id,
+            source_chat_id=message.chat.id,
+            source_message_id=message.id,
+        )
+
+        if not history_obj:
+            raise MessageNotFoundOnHistoryError(operation=EDIT, message=message)
+
+        history_obj.edited_at = message.edit_date
+        history_obj.data.append(dict(source=json.loads(message.__str__())))
+        history_obj.save()
+
+        if (
+            history_obj.source_id != message.chat.id
+            or history_obj.source_message_id != message.id
+        ):
+            raise MessageForwardedFromAnySourceError(operation=EDIT, message=message)
+
+        if not history_obj.category_message_id:
+            raise MessageNotOnCategoryError(operation=EDIT, message=message)
+
+        if not history_obj.category_message_rewritten:
+            raise MessageNotRewrittenError(operation=EDIT, message=message)
+
+        filter_id = get_filter_id_or_none(message=message, source=source)
+        if history_obj.filter_id != filter_id:
+            history_obj.filter_id = filter_id
+            history_obj.save()
+        if filter_id:
+            raise MessageFilteredError(operation=EDIT, message=message)
+
+        # Первый await !
+
+        if message.text:
+            cleanup_message(message=message, source=source, is_media=False)
+            add_header(obj=message, header=Header(message), is_media=False)
+            category_message = await client.edit_message_text(
+                chat_id=history_obj.category_id,
+                message_id=history_obj.category_message_id,
+                text=message.text,
+                parse_mode=None,
+                entities=message.entities,
+                disable_web_page_preview=True,
+            )
+        else:
+            cleanup_message(message=message, source=source, is_media=True)
+            add_header(obj=message, header=Header(message), is_media=True)
+
+            # category_message = await client.edit_message_media(
+            #     chat_id=history_obj.category_id,
+            #     message_id=history_obj.category_message_id,
+            #     media=get_input_media(message=message),
+            # )  # todo теряет caption_entities !
+
+            category_message = await EditMessageMedia.edit_message_media(
+                client,
+                chat_id=history_obj.category_id,
+                message_id=history_obj.category_message_id,
+                media=get_input_media(message=message),
+            )
+
+        logging.info(
+            'Источник %s изменил сообщение %s, оно изменено в категории %s',
+            message.chat.id,
+            message.id,
+            source.category_id,
+        )
+
+        history_obj.data[-1]['category'] = json.loads(category_message.__str__())
+        history_obj.save()
+
+    except MessageBaseError as e:
+        exc = e
+    except pyrogram_errors.MessageNotModified as error:
+        exc = MessageNotModifiedError(operation=EDIT, message=message, error=error)
+    except pyrogram_errors.MessageIdInvalid as error:
+        exc = MessageIdInvalidError(operation=EDIT, message=message, error=error)
+    except (pyrogram_errors.MediaCaptionTooLong, pyrogram_errors.MessageTooLong):
+        exc = MessageTooLongError(operation=EDIT, message=message)
+    except pyrogram_errors.BadRequest as error:
+        exc = MessageBadRequestError(operation=EDIT, message=message, error=error)
+    finally:
+        if blocked:
+            blocked.remove(value=message.id)
+
+        if exc and history_obj:
+            history_obj.data[-1]['exception'] = exc.text
+            history_obj.save()
+
+        await client.read_chat_history(
+            chat_id=source.id,
+            max_id=message.id,
+        )
+
+
+def get_history_obj_or_none(
+    category_id: int,
+    source_chat_id: int,
+    source_message_id: int,
+) -> MessageHistory | None:
+    """Получить объект истории сообщения."""
+    try:
+        mh: type[MessageHistory] = MessageHistory.alias()
+        history_obj = (
+            mh.select()
+            .where(
+                (mh.category_id == category_id)  # Ищем в конкретной категории
+                & (
+                    (mh.source_id == source_chat_id)
+                    & (mh.source_message_id == source_message_id)
+                )  # Только как сообщение источника
+            )
+            .get()
+        )  # Работает по индексам
+
+        return history_obj
+    except DoesNotExist:
+        return  # noqa R502
