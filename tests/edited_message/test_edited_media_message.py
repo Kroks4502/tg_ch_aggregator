f33ca4fc44c42cbd681e8ff9c74fe801906002ee
Kroks <kroks4502@gmail.com>
2023-08-05 08:23:15 +0300

    feat: basic test structure for edited_message handler

---
+++ tests/edited_message/test_edited_media_message.py
@@ -0,0 +1,38 @@
+import logging
+from unittest.mock import AsyncMock
+
+import pytest
+from _pytest.logging import LogCaptureFixture
+from pytest_mock import MockerFixture
+
+from plugins.user.sources_monitoring import edited_message
+from tests.edited_message.utils import (
+    default_edited_message_log_asserts,
+    setup_filtered,
+    setup_source,
+)
+from tests.utils import setup_json_loads
+
+
+@pytest.mark.asyncio
+async def test_edited_message(mocker: MockerFixture, caplog: LogCaptureFixture, client, message):
+    caplog.set_level(logging.DEBUG)
+
+    mocker.patch("plugins.user.sources_monitoring.edited_message.MessageHistory.get_or_none")
+    setup_source(mocker)
+    setup_filtered(mocker, return_value=None)
+
+    mocker.patch("plugins.user.sources_monitoring.edited_message.cleanup_message")
+    mocker.patch("plugins.user.sources_monitoring.edited_message.add_header")
+    mocker.patch("plugins.user.sources_monitoring.edited_message.cut_long_message")
+
+    mocker.patch("plugins.user.sources_monitoring.edited_message.EditMessageMedia.edit_message_media")
+
+    setup_json_loads(mocker)
+
+    client.edit_message_text = AsyncMock()
+
+    await edited_message.edited_message(client=client, message=message)
+
+    assert 'Источник 0 изменил сообщение 0, оно изменено в категории' in caplog.text
+    default_edited_message_log_asserts(caplog=caplog)
---
+++ tests/edited_message/test_not_in_category.py
@@ -0,0 +1,41 @@
+import logging
+from unittest.mock import AsyncMock, MagicMock
+
+import pytest
+from _pytest.logging import LogCaptureFixture
+from pytest_mock import MockerFixture
+
+from plugins.user.sources_monitoring import edited_message
+from tests.edited_message.utils import (
+    default_edited_message_log_asserts,
+    setup_filtered,
+    setup_source,
+)
+from tests.utils import setup_json_loads
+
+
+@pytest.mark.asyncio
+async def test_not_in_category_message(mocker: MockerFixture, caplog: LogCaptureFixture, client, message):
+    caplog.set_level(logging.DEBUG)
+
+    mocker.patch(
+        "plugins.user.sources_monitoring.edited_message.MessageHistory.get_or_none",
+        return_value=MagicMock(category_message_id=None),
+    )
+    setup_source(mocker)
+    setup_filtered(mocker)
+
+    mocker.patch("plugins.user.sources_monitoring.edited_message.cleanup_message")
+    mocker.patch("plugins.user.sources_monitoring.edited_message.add_header")
+    mocker.patch("plugins.user.sources_monitoring.edited_message.cut_long_message")
+
+    mocker.patch("plugins.user.sources_monitoring.edited_message.EditMessageMedia.edit_message_media")
+
+    setup_json_loads(mocker)
+
+    client.edit_message_text = AsyncMock()
+
+    await edited_message.edited_message(client=client, message=message)
+
+    assert 'Источник 0 изменил сообщение 0, оно не публиковалось в категории' in caplog.text
+    default_edited_message_log_asserts(caplog=caplog)
